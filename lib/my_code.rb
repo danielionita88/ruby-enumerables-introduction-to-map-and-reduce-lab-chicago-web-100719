db5e601322d0784f19c37347125730fd3c249311
Daniel I <danielionita88@yahoo.com>
2019-09-18 17:53:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,6 +64,18 @@ def reduce_to_all_true(source_array)
   return true
   
 end
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do
+    if source_array[i] == true
+      return true
+    end
+    i += 1 
+    
+  end
+  return false
+end
     
      
     
